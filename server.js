bf2f473b091ca4a7db53890046c2e9d6d9dfc0d7
Hack Reactor Students <accounts@hackreactor.com>
2015-07-29 15:42:13 -0700

    tell azure to ignore node modules

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = require('./server-config.js');
 var port = process.env.PORT || 4568;
 var ip = process.env.IP || 127.0.0.1;
 
-app.listen(port, ip);
+app.listen(port);
 
 console.log('Server now listening on port ' + port);
